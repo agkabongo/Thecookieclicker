6425ab305a49af0c32e5007b3b9621009832f708
Agnes Kabongo <57146479+agkabongo@users.noreply.github.com>
2021-02-26 17:01:32 +0100

    Show the available upgrades

--- src/Store.js
+++ src/Store.js
@@ -5,12 +5,7 @@ const Store = ({spendOnClick}) => {
     return (
         <div className="justify-center w-2/6">
             <h1 className="text-xl">Upgrades</h1>
-            <Upgrade spendOnClick={spendOnClick} i={1}/>
-            <Upgrade spendOnClick={spendOnClick} i={2}/>
-            <Upgrade spendOnClick={spendOnClick} i={3}/>
-            <Upgrade spendOnClick={spendOnClick} i={4}/>
-            <Upgrade spendOnClick={spendOnClick} i={5}/>
-            
+            <Upgrade spendOnClick={spendOnClick}/> 
         </div>
     )
 
--- src/Upgrades.js
+++ src/Upgrades.js
@@ -1,69 +1,65 @@
 import React from 'react';
 
-
-
 function Upgrade({
     spendOnClick,
     cost,
     name,
-    i,
-    upinfo = {
-        1: {id: 1,
+    upinfo = [
+        {
+            id: 1,
             cps: 1,
             cost: 10,
             name: "auto-clicker",
-            acquired : 0
-          },
-          2: {
+            acquired: 0
+        }, {
             id: 2,
             cps: 5,
             cost: 50,
             name: "grandma",
-            acquired : 0
-          },
-          3: {
+            acquired: 0
+        }, {
             id: 3,
             cps: 25,
             cost: 100,
             name: "shipment",
-            acquired : 0
-          },
-          4: {
+            acquired: 0
+        }, {
             id: 4,
             cps: 100,
             cost: 1000,
             name: "truckload",
-            acquired : 0
-          },
-          5: {
+            acquired: 0
+        }, {
             id: 5,
             cps: 500,
             cost: 10000,
             name: "factory",
-            acquired : 0
-          }
-    }
+            acquired: 0
+        }
+    ]
 
-}) 
-
-
-{
-    return (upinfo[i].acquired++,
+}) {
+    return (
 
         <div>
-           
 
-            <div>
-                {upinfo[i].name} ({upinfo[i].cps}cps): {upinfo[i].cost}c
-              </div>
-              <button
-                className="buyBtn"
-                onClick={() => {spendOnClick(upinfo[i].cost,upinfo[i].cps);}}
-              >
-                Buy
-              </button>
+            {upinfo.map((info) => {
+                return (
+                    <div>
+                        <div>{info.name}{info.cps}cps: {info.cost}c</div>
+                        <button
+                            className="buyBtn"
+                            onClick={() => {
+                            spendOnClick(info.cost, info.cps);
+                        }}>
+                            Buy
+                        </button>
+                    </div>
+                )
+            })}
+
         </div>
-      
+
     );
 }
 
